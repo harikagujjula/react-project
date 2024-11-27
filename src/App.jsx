78ce2ae6e4c04d7babec68206f7ee88396df787b
harika <harika26.gujjula@gmail.com>
2024-11-27 16:02:25 -0500

    Rendering dynamic values.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-function App() {
+import reactImg from "./assets/react-core-concepts.png";
+const reactDescriptions = ["Fundamental", "Core", "Crucial"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const randomDescription = reactDescriptions[getRandomInt(2)];
+
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <header>
+        <img src={ reactImg } alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {/* Using dynamic content render. */}
+          { randomDescription } React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
