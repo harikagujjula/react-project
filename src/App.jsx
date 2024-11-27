cc57596766d637ba8f2d4cc8b67e23ad9bb6bbc9
harika <harika26.gujjula@gmail.com>
2024-11-27 16:18:24 -0500

    Using Props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
 const reactDescriptions = ["Fundamental", "Core", "Crucial"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcepts (props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <title>{props.title}</title>
+      <description> {props.description} </description>
+    </li>
+  );
 }
 
 function Header() {
@@ -20,12 +32,21 @@ function Header() {
       </header>
   );
 }
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+        </ul>
+        </section>
       </main>
     </div>
   );
