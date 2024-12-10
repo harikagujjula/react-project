39e6b7fd5e67b17e69a9460a1adc78f00734b7db
harika <harika26.gujjula@gmail.com>
2024-12-10 18:39:07 -0500

    6.Styling React components - Updating the styles to include as a component.

--- src/components/StylingReactComponents/Header.jsx
+++ src/components/StylingReactComponents/Header.jsx
@@ -2,10 +2,10 @@ import logo from '../../assets/styling-react-components-logo.png';
 
 export default function Header() {
   return (
-    <header>
+    <div id="header">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </div>
   );
 }
--- src/components/StylingReactComponents/StylingReactComponents.jsx
+++ src/components/StylingReactComponents/StylingReactComponents.jsx
@@ -1,10 +1,11 @@
 import AuthInputs from './AuthInputs.jsx';
 import Header from './Header.jsx';
+// Note that the Css files can be imported/included and these will be dynamically added to the webpage by vite.
 import './styling-react-components.css';
 
 export default function StylingReactComponents() {
   return (
-    <div id="styling-react-comps">
+    <div id='styling-react-comps'>
       <Header />
       <main>
         <AuthInputs />
