fd8daa6e5d21f3bccb7d87d6b71d84fb5c06c66f
harika <harika26.gujjula@gmail.com>
2024-12-20 18:21:22 -0500

    10. Outsourcing Context and State into a seperate Provider component.

--- src/components/OnlineShoppingDemo/OnlineShoppingDemo.jsx
+++ src/components/OnlineShoppingDemo/OnlineShoppingDemo.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import OSHeader from './OSHeader.jsx';
 import Shop from './Shop.jsx';
 import Product from './Product.jsx';
 import './OnlineShoppingDemo.css';
+import CartContextProvider from '../../store/shopping-cart-context.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import { CartContext } from '../../store/shopping-cart-context.jsx';
 
 /* 
   This setup is similar to Project management where a state is being shared 
@@ -12,75 +11,7 @@ import { CartContext } from '../../store/shopping-cart-context.jsx';
   Ex: Props being forwarded from OnlineShoppingDemo > Shop > Product and 
   similarly OnlineShoppingDemo > OSHeader > CartModal > Cart. */
 export default function OnlineShoppingDemo() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
 
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  // Passing not just the Readable variables but also to update variables using context.
-  const ctxValue = {
-    items: shoppingCart.items,
-    /* Passing handleAddItemToCart function as a value so that we can get rid of
-     the code where we are sending this function as props and instead access 
-     using context.*/
-    addItemToCart: handleAddItemToCart,
-    updateItemQuantity: handleUpdateCartItemQuantity
-  }
   return (
     /* Wrapping all the components that need the CartContext created with 
     context as a component(Supportted by React >= 19).
@@ -90,7 +21,24 @@ export default function OnlineShoppingDemo() {
     default value set when creating context(in shopping-cart-context.jsx) is 
     only used if a component that was not wrapped by <CartContext> tries to 
     access the context value.*/
-    <CartContext value={ctxValue}>
+    // <CartContext value={ctxValue}>
+    //   <OSHeader />
+    //   {/*  Component composition: Wrapping a component and rendering using 
+    //   children. No need of passing props anymore as we moved Product rendering 
+    //   logic here which needs those props. */}
+    //   {/* <Shop onAddItemToCart={handleAddItemToCart} /> */}
+    //   <Shop>
+    //     {DUMMY_PRODUCTS.map((product) => (
+    //       <li key={product.id}>
+    //         <Product {...product} />
+    //       </li>
+    //     ))}
+    //   </Shop>
+    // </CartContext>
+
+    // Using the Custom CartContextProvider component, so that all context/state
+    // data is at one place.
+    <CartContextProvider>
       <OSHeader />
       {/*  Component composition: Wrapping a component and rendering using 
       children. No need of passing props anymore as we moved Product rendering 
@@ -103,6 +51,6 @@ export default function OnlineShoppingDemo() {
           </li>
         ))}
       </Shop>
-    </CartContext>
+    </CartContextProvider>
   );
 }
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { DUMMY_PRODUCTS } from '../components/OnlineShoppingDemo/dummy-products.js';
+
+
 
 /* Creating a context with an initial value(of any type, object here) which 
   contains React component that we would need. Having default value defined 
@@ -13,3 +16,83 @@ export const CartContext = createContext({
   updateItemQuantity: () => {}
 });
 
+/* 
+  Moving all the Context/State related code in to this function to prevent it 
+  sitting at one place that could be cumbersome when we have more contexts and 
+  more states. */
+export default function CartContextProvider({children}) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  // Passing not just the Readable variables but also to update variables using context.
+  const ctxValue = {
+    items: shoppingCart.items,
+    /* Passing handleAddItemToCart function as a value so that we can get rid of
+     the code where we are sending this function as props and instead access 
+     using context.*/
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity
+  }
+
+  // Returning the CartContext with Children.
+  return <CartContext.Provider value={ctxValue}>
+    {children}
+  </CartContext.Provider>
+}
